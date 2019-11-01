75f9b349ee3327f52dc9c410ce776c594b803aa9
adam <adam.thorpe.g@gmail.com>
2019-11-01 17:38:06 -0600

    Adding dummy test.

---
+++ test/states/test_states.cc
@@ -0,0 +1,10 @@
+#include <catch2/catch.hpp>
+
+#include <symctrl/states/state.hpp>
+#include <symctrl/states/vector.hpp>
+
+using namespace symctrl;
+
+TEST_CASE("States: operations", "[states]") {
+
+}
